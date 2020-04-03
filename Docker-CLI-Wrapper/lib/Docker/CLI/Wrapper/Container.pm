df1c46ff456a8683225e21d20d13493315dc839c
Shlomi Fish <shlomif@shlomifish.org>
2020-04-03 20:45:32 +0300

    Extract a common module/library/header.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

---
+++ Docker-CLI-Wrapper/lib/Docker/CLI/Wrapper/Container.pm
@@ -0,0 +1,51 @@
+package Docker::CLI::Wrapper::Container;
+
+use strict;
+use warnings;
+use 5.014;
+use autodie;
+
+use Moo;
+use Path::Tiny qw/ path /;
+
+extends('Docker::CLI::Wrapper::Base');
+
+has 'sys' => ( is => 'ro', required => 1, );
+
+1;
+
+__END__
+
+=head1 NAME
+
+Docker::CLI::Wrapper::Container - manage a container.
+
+=head1 SYNOPSIS
+
+    use Docker::CLI::Wrapper::Container ();
+
+    my $obj = Docker::CLI::Wrapper::Container->new(
+        {
+            sys => 'debian:sid',
+        }
+    );
+
+=head1 METHODS
+
+=head2 $obj->sys()
+
+The desired operating system / docker image to use for the container.
+A string.
+=head2 $obj->do_system({ cmd => [@CMD]});
+
+Sugar for system(@CMD) - prints and dies on error.
+
+=head2 $obj->docker({ cmd => [@CMD]});
+
+Runs docker using the args in @CMD, using do_system.
+
+=head2 $obj->calc_docker_cmd_line_prefix()
+
+Calculates the prefix for running docker at the attributes' init time.
+
+=cut
