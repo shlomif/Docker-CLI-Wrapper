95e518b8f700ae6ce05366ab47e25fe01e7cb067
Shlomi Fish <shlomif@shlomifish.org>
2020-04-03 19:40:21 +0300

    add more code

--- Docker-CLI-Wrapper/lib/Docker/CLI/Wrapper/Base.pm
+++ Docker-CLI-Wrapper/lib/Docker/CLI/Wrapper/Base.pm
@@ -4,6 +4,10 @@ use strict;
 use warnings;
 
 use Moo;
+use Path::Tiny qw/ path tempdir tempfile cwd /;
+
+has 'docker_cmd_line_prefix' =>
+    ( is => 'ro', lazy => 1, builder => 'calc_docker_cmd_line_prefix' );
 
 sub do_system
 {
@@ -19,6 +23,37 @@ sub do_system
     return;
 }
 
+sub calc_docker_cmd_line_prefix
+{
+    my $self = shift;
+
+    {
+        my $fh = path("/etc/fedora-release");
+
+        if ( -e $fh )
+        {
+            if ( my ($fedora_ver) =
+                $fh->slurp_utf8() =~ /^Fedora release ([0-9]+)/ )
+            {
+                if ( $fedora_ver >= 31 )
+                {
+                    # return ['podman'];
+                    return [ 'systemd-run', '--scope', '--user', 'podman' ];
+                }
+            }
+        }
+    }
+    return ['docker'];
+}
+
+sub docker
+{
+    my ( $self, $args ) = @_;
+
+    my $cmd = $args->{cmd};
+    return $self->do_system(
+        { %$args, cmd => [ @{ $self->docker_cmd_line_prefix }, @$cmd, ], } );
+}
 1;
 
 __END__
@@ -32,5 +67,13 @@ Docker::CLI::Wrapper::Base - base class.
 =head2 $obj->do_system({ cmd => [@CMD]});
 
 Sugar for system(@CMD) - prints and dies on error.
+
+=head2 $obj->docker({ cmd => [@CMD]});
+
+Runs docker using the args in @CMD, using do_system.
+
+=head2 $obj->calc_docker_cmd_line_prefix()
+
+Calculates the prefix for running docker at the attributes' init time.
 
 =cut
